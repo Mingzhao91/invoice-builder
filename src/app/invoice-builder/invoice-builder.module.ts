50f6a02e7c2f822e896b2697ce714c33c75528ea
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 21:17:16 +0000

    init side nav component

--- src/app/invoice-builder/invoice-builder.module.ts
+++ src/app/invoice-builder/invoice-builder.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { InvoiceBuilderRoutingModule } from './invoice-builder-routing.module';
 import { InvoiceBuilderComponent } from './invoice-builder.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
 
 
 @NgModule({
   declarations: [
     InvoiceBuilderComponent,
-    MainContentComponent
+    MainContentComponent,
+    SideNavComponent
   ],
   imports: [
     CommonModule,
