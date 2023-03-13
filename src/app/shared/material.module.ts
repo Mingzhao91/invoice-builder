b3d0c2d756c37bdee6b58b8507025ad41cf4c0e1
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 21:45:43 +0000

    button and sidenav module in material module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatButtonModule, MatSidenavModule],
+  exports: [MatButtonModule, MatSidenavModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
