95ce54d4149abbed4b4cfe83f224edfe1f69d721
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 21:46:09 +0000

    import material module in invoice builder module

--- src/app/invoice-builder/invoice-builder.module.ts
+++ src/app/invoice-builder/invoice-builder.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MaterialModule } from '../shared/material.module';
+
 import { InvoiceBuilderRoutingModule } from './invoice-builder-routing.module';
 import { InvoiceBuilderComponent } from './invoice-builder.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
-
 @NgModule({
   declarations: [
     InvoiceBuilderComponent,
     MainContentComponent,
     SideNavComponent,
-    ToolbarComponent
+    ToolbarComponent,
   ],
-  imports: [
-    CommonModule,
-    InvoiceBuilderRoutingModule
-  ]
+  imports: [CommonModule, MaterialModule, InvoiceBuilderRoutingModule],
 })
-export class InvoiceBuilderModule { }
+export class InvoiceBuilderModule {}
