cfef5fbe71cf4427f076bf84419af8a97b5cc085
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 21:18:11 +0000

    init toolbar component

--- src/app/invoice-builder/invoice-builder.module.ts
+++ src/app/invoice-builder/invoice-builder.module.ts
@@ -5,13 +5,15 @@ import { InvoiceBuilderRoutingModule } from './invoice-builder-routing.module';
 import { InvoiceBuilderComponent } from './invoice-builder.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 
 @NgModule({
   declarations: [
     InvoiceBuilderComponent,
     MainContentComponent,
-    SideNavComponent
+    SideNavComponent,
+    ToolbarComponent
   ],
   imports: [
     CommonModule,
