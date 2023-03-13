b52fd761d2dd3e1dd08097cc38e7aa0e96224050
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 21:00:58 +0000

    lazy load invoice module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'invoice-builder',
+    loadChildren: () =>
+      import('./invoice-builder/invoice-builder.module').then(
+        (m) => m.InvoiceBuilderModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'invoice-builder',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
