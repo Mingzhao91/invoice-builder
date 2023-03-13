4bfa16b1d8d65de08ab44095e9884420ef48aff8
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 22:30:27 +0000

    add toolbar and list modules in material module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatSidenavModule],
-  exports: [MatButtonModule, MatSidenavModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+  ],
+  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatListModule],
 })
 export class MaterialModule {}
