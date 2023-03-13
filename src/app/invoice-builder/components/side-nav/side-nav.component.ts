a05bcd910a1f9d9fd978c461d8dc70470c41d324
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-13 23:27:56 +0000

    toggle side menu based on screen width

--- src/app/invoice-builder/components/side-nav/side-nav.component.ts
+++ src/app/invoice-builder/components/side-nav/side-nav.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import { MatSidenav } from '@angular/material/sidenav';
+
+const MAX_WIDTH_BREAKPOINT = 768;
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
 
-  constructor() { }
+  constructor(private observer: BreakpointObserver) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.observer
+      .observe([`(max-width: ${MAX_WIDTH_BREAKPOINT}px)`])
+      .subscribe((resp) => {
+        this.sidenav.mode = resp.matches ? 'over' : 'side';
+        this.sidenav[resp.matches ? 'close' : 'open']();
+      });
   }
-
 }
