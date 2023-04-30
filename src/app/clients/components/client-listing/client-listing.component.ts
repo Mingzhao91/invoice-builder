bd8be84eefe39cef1cbd73d7785cfd891881747f
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-04-30 14:46:49 +0100

    display a table to show all clients

--- src/app/clients/components/client-listing/client-listing.component.ts
+++ src/app/clients/components/client-listing/client-listing.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
+
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/client';
 
 @Component({
   selector: 'app-client-listing',
   templateUrl: './client-listing.component.html',
-  styleUrls: ['./client-listing.component.scss']
+  styleUrls: ['./client-listing.component.scss'],
 })
 export class ClientListingComponent implements OnInit {
+  subscription: Subscription = new Subscription();
+  isResultsLoading = false;
+  dataSource: Client[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'action'];
 
-  constructor() { }
+  constructor(
+    private snackBar: MatSnackBar,
+
+    private clientService: ClientService
+  ) {}
 
   ngOnInit(): void {
+    this.getClients();
   }
 
+  getClients() {
+    this.isResultsLoading = true;
+    this.subscription.add(
+      this.clientService.getClients().subscribe({
+        next: (data) => {
+          this.dataSource = data;
+          this.isResultsLoading = false;
+        },
+        error: (err) => {
+          this.openSnackBar('Failed to retrieve invoices!', 'Error');
+          this.isResultsLoading = false;
+        },
+      })
+    );
+  }
+
+  saveBtnHanlder() {}
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action);
+  }
 }
