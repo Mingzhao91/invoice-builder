7d3316ec7012fad691a2308a15e426b1f3eebd10
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-04-30 11:11:19 +0100

    CRUD operations for invoice

--- backend/src/api/resources/client/client.controller.js
+++ backend/src/api/resources/client/client.controller.js
@@ -14,7 +14,70 @@ export default {
       const client = await Client.create(value);
       return res.json(client);
     } catch (err) {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+  },
+
+  async findAll(req, res) {
+    try {
+      const client = await Client.find();
+      return res.json(client);
+    } catch (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+  },
+
+  async findOne(req, res) {
+    try {
+      const { id } = req.params;
+      const client = await Client.findById(id);
+
+      if (!client) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ err: "Could not find a client" });
+      }
+
+      return res.json(client);
+    } catch (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const client = await Client.findByIdAndRemove(id);
+
+      if (!client) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ err: "Conld not delete a client" });
+      }
+
+      return res.json(client);
+    } catch (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+  },
+
+  async update(req, res) {
+    try {
+      const { value, error } = clientService.validateCreateSchema(req.body);
+      if (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json(error);
+      }
+      const { id } = req.params;
+      const client = await Client.findByIdAndUpdate(id, value, { new: true });
+
+      if (!client) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ err: "Could not update client" });
+      }
+      return res.json(client);
+    } catch (err) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR);
     }
   },
 };
--- backend/src/api/resources/client/client.router.js
+++ backend/src/api/resources/client/client.router.js
@@ -4,4 +4,13 @@ import clientController from "./client.controller";
 
 export const clientRouter = express.Router();
 
-clientRouter.route("/").post(clientController.create);
+clientRouter
+  .route("/")
+  .post(clientController.create)
+  .get(clientController.findAll);
+
+clientRouter
+  .route("/:id")
+  .get(clientController.findOne)
+  .delete(clientController.delete)
+  .put(clientController.update);
