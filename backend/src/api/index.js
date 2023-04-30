e4d947c913d925058d474dccb7a65b6db78cfcc2
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-04-30 07:29:05 +0100

    use client router

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 
 import { invoiceRouter } from "./resources/invoice";
+import { clientRouter } from "./resources/client";
 
 export const restRouter = express.Router();
 
 restRouter.use("/invoices", invoiceRouter);
+restRouter.use("/clients", clientRouter);
