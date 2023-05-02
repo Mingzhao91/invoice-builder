63d97c555cd304f66af6ff0955098c6ec0c8b486
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-02 20:40:16 +0100

    create user model

---
+++ backend/src/api/resources/user/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
