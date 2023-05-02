78149fc5b0b999f9acfe377c8d899ada68b64cb0
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-02 21:19:48 +0100

    handle user login

--- backend/src/api/resources/user/user.controller.js
+++ backend/src/api/resources/user/user.controller.js
@@ -1,4 +1,6 @@
 import { StatusCodes } from "http-status-codes";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 import userService from "./user.service";
 import User from "./user.model";
@@ -13,6 +15,37 @@ export default {
 
       const user = await User.create(value);
       return res.json({ success: true, message: "User create successfully" });
+    } catch (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
+    }
+  },
+
+  async login(req, res) {
+    try {
+      const { value, error } = userService.validateSchema(req.body);
+      if (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json(error);
+      }
+
+      const user = await User.findOne({ email: value.email });
+
+      console.log("user: ", user);
+
+      if (!user) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ err: "Invalid emai or password." });
+      }
+
+      const matched = await bcryptjs.compare(value.password, user.password);
+
+      if (!matched) {
+        res
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({ err: "invalid credential" });
+      }
+      const token = jwt.sign({ id: user._id }, "123456", { expiresIn: "1d" });
+      return res.json({ success: true, token });
     } catch (err) {
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
     }
--- backend/src/api/resources/user/user.router.js
+++ backend/src/api/resources/user/user.router.js
@@ -4,3 +4,4 @@ import userController from "./user.controller";
 export const userRounter = express.Router();
 
 userRounter.post("/signup", userController.signup);
+userRounter.post("/login", userController.login);
