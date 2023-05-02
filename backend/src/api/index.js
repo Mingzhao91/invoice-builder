e95032c55a288b08d8eccb51bf080506652d0d13
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-02 20:33:32 +0100

    define user routes

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 import { invoiceRouter } from "./resources/invoice";
 import { clientRouter } from "./resources/client";
+import { userRounter } from "./resources/user";
 
 export const restRouter = express.Router();
 
 restRouter.use("/invoices", invoiceRouter);
 restRouter.use("/clients", clientRouter);
+restRouter.use("/users", userRounter);
---
+++ backend/src/api/resources/user/user.router.js
@@ -0,0 +1,6 @@
+import express from "express";
+import userController from "./user.controller";
+
+export const userRounter = express.Router();
+
+userRounter.post("/signup", userController.signup);
