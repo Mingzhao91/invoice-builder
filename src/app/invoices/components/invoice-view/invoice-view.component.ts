8487d664052b2c79df5457bb21b300a3fd6a742b
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-13 17:21:43 +0100

    calculate invoice total in view mode

--- src/app/invoices/components/invoice-view/invoice-view.component.ts
+++ src/app/invoices/components/invoice-view/invoice-view.component.ts
@@ -10,6 +10,7 @@ import { Invoice } from '../../models/invoice';
 })
 export class InvoiceViewComponent implements OnInit {
   invoice!: Invoice;
+  total!: number;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,6 +18,20 @@ export class InvoiceViewComponent implements OnInit {
     this.route.data.subscribe(({ invoice }) => {
       console.log('invoice: ', invoice);
       this.invoice = invoice;
+
+      if (
+        typeof this.invoice.qty !== 'undefined' &&
+        typeof this.invoice.rate !== 'undefined'
+      ) {
+        this.total = this.invoice.qty * this.invoice.rate;
+      }
+
+      let salesTax = 0;
+      if (typeof this.invoice.tax !== 'undefined') {
+        salesTax = (this.total * this.invoice.tax) / 100;
+      }
+
+      this.total += salesTax;
     });
   }
 }
